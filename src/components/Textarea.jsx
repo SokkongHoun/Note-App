4ebc9aaebdd14b730d2a0fa7654f8ffc3757de3f
Sokkong <sokkonghoun1@gmail.com>
2024-03-08 15:08:16 +0700

    implement ReactQuill for text editors

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,51 +1,39 @@
 import React from "react";
-import EditorNavbar from "./EditorNav";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css"; // import styles
 
 export function NotesArea(props) {
   const [notes, setNotes] = React.useState("");
   const [currentNote, setCurrentNote] = React.useState(null);
-
   React.useEffect(() => {
     const activeNote = props.activeNote;
     const createdNote = props.createdNote;
     const foundNote = createdNote.find((note) => note.id === activeNote);
     setCurrentNote(() => foundNote);
+
     if (foundNote) {
       setNotes(foundNote.task);
     }
   }, [props.activeNote, props.createdNote]);
 
-  React.useEffect(() => {
-    if (currentNote) {
-      console.log(currentNote.task);
-    }
-  }, [currentNote]);
-
-  function handleOnChanges(event) {
-    setNotes(event.target.value);
+  function handleOnChanges(content, delta, source, editor) {
+    setNotes(editor.getHTML());
 
     const updatedNotes = props.createdNote.map((note) =>
-      note.id === props.activeNote
-        ? { ...note, task: event.target.value }
-        : note
+      note.id === props.activeNote ? { ...note, task: editor.getHTML() } : note
     );
     props.setCreatedNotes(updatedNotes);
   }
 
   return (
-    <div className="h-full flex flex-col rounded-r-lg">
-      <EditorNavbar />
-      <div className="rounded-r-lg flex-1 overflow-auto  bg-gray-50 dark:bg-gray-700 ">
-        <div className="px-4 py-2 bg-white dark:bg-gray-800 h-full">
-          <textarea
-            id="comment"
-            className="w-full h-full px-3 py-3 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
-            placeholder="Add your notes..."
-            value={notes}
-            onChange={handleOnChanges}
-          />
-        </div>
-      </div>
+    <div className="rounded-r-lg px-4 py-2 bg-white h-full text-black">
+      <ReactQuill
+        style={{ height: "94.5%", borderRadius: 10 }}
+        value={notes}
+        onChange={handleOnChanges}
+      />
     </div>
   );
 }
+
+export default NotesArea;
